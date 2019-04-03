1ef12de109605a61fa5b659d093ad239f104aa6f
Enrique Gonzalez <enriikke@gmail.com>
2019-04-03 09:18:30 -0400

    Set up gatsby

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,53 +1,99 @@
-/**
- * Layout component that queries for data
- * with Gatsby's StaticQuery component
- *
- * See: https://www.gatsbyjs.org/docs/static-query/
- */
+import React from 'react'
+import PropTypes from 'prop-types'
+import { Helmet } from 'react-helmet';
+import { createGlobalStyle, ThemeProvider } from 'styled-components';
+import { StaticQuery, graphql } from 'gatsby'
 
-import React from "react"
-import PropTypes from "prop-types"
-import { StaticQuery, graphql } from "gatsby"
+const LayoutStyle = createGlobalStyle`
+*,
+*::after,
+*::before {
+  -webkit-box-sizing: inherit;
+  box-sizing: inherit;
+}
 
-import Header from "./header"
-import "./layout.css"
+body {
+  -webkit-box-sizing: border-box;
+  box-sizing: border-box;
+  margin: 0;
+  font-family: Cabin;
+  overflow-x: hidden;
+}
+`;
 
-const Layout = ({ children }) => (
-  <StaticQuery
-    query={graphql`
-      query SiteTitleQuery {
-        site {
-          siteMetadata {
-            title
+const LayoutQuery = graphql`
+  query LayoutQuery {
+    site {
+      siteMetadata {
+        title
+        description
+        author
+        profile {
+          favicon16: resize {
+            src
+          }
+          favicon32: resize {
+            src
+          }
+          bigIcon: resize {
+            src
+          }
+          appleIcon: resize {
+            src
           }
         }
       }
-    `}
-    render={data => (
-      <>
-        <Header siteTitle={data.site.siteMetadata.title} />
-        <div
-          style={{
-            margin: `0 auto`,
-            maxWidth: 960,
-            padding: `0px 1.0875rem 1.45rem`,
-            paddingTop: 0,
-          }}
-        >
-          <main>{children}</main>
-          <footer>
-            © {new Date().getFullYear()}, Built with
-            {` `}
-            <a href="https://www.gatsbyjs.org">Gatsby</a>
-          </footer>
-        </div>
-      </>
-    )}
+    }
+  }
+`;
+
+const Layout = ({ children }) => (
+  <StaticQuery
+    query={LayoutQuery}
+    render={data => {
+      const lang = 'en';
+      const { title, description, author, profile } = data.site.siteMetadata;
+
+      return (
+        <React.Fragment>
+          <Helmet htmlAttributes={{ lang, }}>
+            <meta charSet="utf-8" />
+            <title>{title}</title>
+            <meta name="description" content={description} />
+            <link rel="shortcut icon" href={`https:${profile.favicon32.src}`} />
+
+            <meta property="og:title" content={title} />
+            <meta property="og:site_name" content={title} />
+            <meta property="og:description" content={description} />
+            <meta property="og:type" content="website" />
+            <meta property="og:locale" content="en_US" />
+            <meta property="og:url" content="http://www.imdb.com/title/tt0117500/" />
+            <meta property="og:image" content={`https:${profile.bigIcon.src}`} />
+
+            <meta name="twitter:card" content="summary" />
+            <meta name="twitter:title" content={title} />
+            <meta name="twitter:creator" content={author} />
+            <meta name="twitter:description" content="{description}" />
+            <meta name="twitter:image" content={`https:${profile.bigIcon.src}`} />
+            <meta name="twitter:image:src" content={`https:${profile.bigIcon.src}`} />
+
+            <link rel="apple-touch-icon" sizes="180x180" href={`https:${profile.appleIcon.src}`} />
+            <link rel="icon" type="image/png" sizes="32x32" href={`https:${profile.favicon32.src}`} />
+            <link rel="icon" type="image/png" sizes="16x16" href={`https:${profile.favicon16.src}`} />
+          </Helmet>
+
+          <LayoutStyle />
+          <ThemeProvider theme={{}}>
+            {children}
+          </ThemeProvider>
+        </React.Fragment>
+      );
+    }}
   />
-)
+);
 
 Layout.propTypes = {
   children: PropTypes.node.isRequired,
-}
+};
 
-export default Layout
+export default Layout;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,20 +1,47 @@
 import React from "react"
-import { Link } from "gatsby"
+import styled from 'styled-components';
+import { Heading } from 'rebass';
+import Layout from "../components/Layout"
 
-import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
+const SectionContainer = styled.div`
+  min-height: 100vh;
+  min-width: 320px;
+  max-width: 1366px;
+  display: flex;
+  margin: auto;
+  flex: 0 1 auto;
+  flex-direction: column;
+  justify-content: center;
+  padding: 5em 1em;
+  scroll-behavior: smooth;
+`;
+
+
+
 
 const IndexPage = () => (
   <Layout>
-    <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
+    <SectionContainer>
+      <Heading
+        textAlign="center"
+        as="h1"
+        color="primary"
+        fontSize={[5, 6, 8]}
+        mb={[3, 4, 5]}
+      >
+        Hola, I'm Enrique!
+      </Heading>
+
+      <Heading
+        textAlign="center"
+        as="h2"
+        color="primary"
+        fontSize={[4, 5, 6]}
+        mb={[3, 5]}
+      >
+        I like to build stuff
+      </Heading>
+    </SectionContainer>
   </Layout>
 )
 
